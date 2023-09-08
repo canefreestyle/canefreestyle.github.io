2032cca2ba82b02e67dd83348983be2dee2c97d4
Drew Brown <drew.brownjr@gmail.com>
2023-09-08 02:59:14 -0400

    Added Youtube links

--- files/clean_links.sh
+++ files/clean_links.sh
@@ -10,18 +10,19 @@ output_file="modified_links.txt"
 # Remove trailing "?" characters at the end of each line
 # Replace "&pp=<VARIABLE STRING>" with "?"
 # Replace "%3D" with ""
-sed -e 's|https://www.youtube.com/watch?v=|https://youtu.be/|g' \
+sed -e 's|www\.||' \
+    -e 's|https://youtube.com/shorts/|https://youtu.be/|g' \
+    -e 's|https://youtube.com/watch?v=|https://youtu.be/|g' \
     -e 's|%3D||g' \
-    -e 's|reel\/|p\/|g' \
-    -e 's|www.||' \
+    -e 's|reel/|p/|g' \
     -e 's|&index=[^&]*|?|g' \
-    -e 's|\&list=[^&]*||g' \
+    -e 's|&list=[^&]*||g' \
     -e 's|\?list=[^&]*|?|g' \
-    -e 's|\?list=[^&]*|?|g' \
-    -e 's|\&t=|\?t=|g' \
+    -e 's|&t=|\?t=|g' \
     -e 's|\??t=|\?t=|g' \
     -e 's|\?$||' \
-    -e 's|&pp=[^&]*|?|g' "$input_file" > "$output_file"
+    -e 's|&pp=[^&]*|?|g' \
+    -e 's/[[:space:]]*$//' "$input_file" > "$output_file"
 
 # Remove trailing blank lines from the output file
 sed -i '/^$/d' "$output_file"
@@ -37,5 +38,8 @@ mv "sorted_and_unique_links.txt" "$input_file"
 
 # Remove the intermediate modified file
 rm "$output_file"
+
+# # Use awk to remove duplicates and perform other modifications
+# awk -F'[?&]' '{gsub("https://www.youtube.com/watch?v=", "https://youtu.be/", $1); gsub(/%3D|reel\//, "", $1); gsub("www.", "", $1); gsub(/&index=[^&]*/, "?", $2); gsub(/&list=[^&]*/, "", $2); gsub(/\?list=[^&]*/, "?", $2); gsub(/&t=|\??t=/, "?t=", $2); gsub(/\?$/, "", $2); gsub(/&pp=[^&]*/, "?", $2); $1=$1; print $1 "?" $2}' "$input_file" | sort -u > "$output_file"
 
 echo "Modifications complete."
