b29b6acadba6f7f530053e1c59dab556bb60e139
Drew Brown <drew.brownjr@gmail.com>
2024-01-08 21:33:47 -0500

    Added working random_redirect files

---
+++ files/random_redirect.js
@@ -0,0 +1,69 @@
+async function countClicks() {
+    // Retrieve the counter value from local storage or initialize it to 0
+    let counter = parseInt(localStorage.getItem('counter')) || 0;
+
+    // Increment the counter each time the page is visited
+    counter++;
+
+    // After counter reaches 10, reset it to 1 (to show an ad link every 10 visits)
+    if (counter > 10) {
+        counter = 1;
+    }
+
+    // Save the updated counter value in local storage
+    localStorage.setItem('counter', counter);
+
+    return counter;
+}
+
+async function setLinkList() {
+    let link_list;
+    let counter = await countClicks();
+
+    if (counter === 1) {
+        link_list = 'https://canefreestyle.com/files/ad_links.txt';
+    } else {
+        link_list = 'https://canefreestyle.com/files/cane_links.txt';
+    }
+
+    return link_list;
+}
+
+async function getRandomURL() {
+    // Fetch the content of the specified link_list file
+    let link_list = await setLinkList();
+    try {
+        const response = await fetch(link_list);
+
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status}`);
+        }
+
+        const data = await response.text();
+
+        // Select a random URL from the link_list
+        const urlsArray = data.trim().split('\n').map(url => url.trim());
+        const randomIndex = Math.floor(Math.random() * urlsArray.length);
+
+        return urlsArray[randomIndex].toString();
+    } catch (error) {
+        // Display the error in the mainContainer element
+        document.getElementById("mainContainer").innerHTML = `Error: ${error.message}`;
+
+        // Return null to indicate an error
+        return null; 
+    }
+}
+
+async function displayRandomURL() {
+    let randomURL = await getRandomURL();
+
+    if (randomURL) {
+        // Redirect to randomURL
+        window.location.href = randomURL;
+
+        // document.getElementById("mainContainer").innerHTML = `randomURL: ${randomURL}`;
+    }
+}
+
+displayRandomURL();
